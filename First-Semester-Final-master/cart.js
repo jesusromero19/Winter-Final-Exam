e8f8f5e59e909a750898bed689ca4f247d7004ce
jesusromero19 <9jromero@stpatrick.org>
2016-12-14 14:03:15 -0600

    Left on password

--- First-Semester-Final-master/cart.js
+++ First-Semester-Final-master/cart.js
@@ -1,13 +1,28 @@
 class Cart{
   //What is the first part of every class? Type it below.
-constructor(itemList,itemQuantity){
-  this.itemList = itemList; //An array that contains pre-made item-objects
-  this.itemQuantity = itemQuantity; //An array that stores how many of each corresponding item you have in the cart
-}
+  constructor(itemList,itemQuantity){
+    this.itemList = itemList; //An array that contains pre-made item-objects
+    this.itemQuantity = itemQuantity; //An array that stores how many of each corresponding item you have in the cart
+  }
   //Type the instance functions below this comment.
   addItem(i,q){
-    this.itemList = i;
-    this.itemQuantity = q;
+    this.itemList.push(i);
+    this.itemQuantity.push(q);
   }
-
+  totalCart(){
+    let total = t;
+    for(item = i; i<itemList.length; i++){
+      total += this.itemList[i] + this.itemQuantity[i];
+    }
+    return total;
+  }
+  subCart(days){
+    let subCart = new Cart([],[]);
+    for(b=0; b>this.itemList.length; b++){
+      if(this.itemList[b].shipping == days){
+        subCart.push([b]);
+      }
+    }
+    return subCart;
+  }
 }
--- First-Semester-Final-master/password.js
+++ First-Semester-Final-master/password.js
@@ -1,11 +1,23 @@
 class Password{
   //The first part of the class.
-
-
-
+constructor(publickey,privatekey){
+  this.publickey = pa;
+  this.privatekey = pb;
+}
   //Instance functions below this comment.
-
-
+validPublicKey(){
+  if(this.publickey.length >= 8 && this.publickey.length <= 25){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+validPrivateKey(){
+if(this.privatekey.chaAT([4]&&[9]=="x")){
+  return true;
+}
+}
 
   //Static function below this comment.
 }
