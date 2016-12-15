78e1ef8a76eefeef6b3411448c8d248ab875afcd
jesusromero19 <9jromero@stpatrick.org>
2016-12-15 13:52:08 -0600

    Almost done

---
+++ Winter-Final-Exam-master/First-Semester-Final-master/password.js
@@ -0,0 +1,31 @@
+class Password{
+  //The first part of the class.
+  constructor(publickey,privatekey){
+    this.publickey = pa;
+    this.privatekey = pb;
+  }
+  //Instance functions below this comment.
+  validPublicKey(){
+    if(this.publickey.length >= 8 && this.publickey.length <= 25){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  validPrivateKey(){
+    if(this.privatekey.chaAT([4]&&[9] == "-")){
+      return true;
+    }
+    else{
+      return false;
+    }
+
+  }
+  //Static function below this comment.
+  static makePrivateKey(){
+    let key = "";
+    let limit = 14;
+    for(let p = 0;)
+  }
+}
