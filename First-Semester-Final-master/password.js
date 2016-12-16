d6ef54c8456f56e2c5685476d3767f286e728515
jesusromero19 <9jromero@stpatrick.org>
2016-12-16 15:25:47 -0600

    finally done

--- First-Semester-Final-master/password.js
+++ First-Semester-Final-master/password.js
@@ -14,34 +14,35 @@ class Password{
     }
   }
   validPrivateKey(){
-    let part1 = this.privatekey.substring(0,4);
-    let part2 = this.privatekey.substring(5,9);
-    let part3 = this.privatekey.substring(10,14);
-
-    if(isNAN(part1))
-
-
-    if(this.privatekey.charAT(4) == "-" && this.privatekey.charAT(9) == "-"){
-      return true;
-    }
-    else{
+    for(let j = 0; j<this.privatekey.length; j++){
+      if(this.privatekey.charAT(4) != "-" && this.privatekey.charAT(9) != "-")
       return false;
     }
-
-  }
-  //Static function below this comment.
-  static makePrivateKey(){
-    let key = "";
-    let limit = 14;
-    let group1 = "";
-    let group2 = "";
-    let group3 = "";
-    for(let p = 0; p<4; p++){
-      group1 = String(Math.floor(Math.random()*10));
-      group2 = String(Math.floor(Math.random()*10));
-      group3 = String(Math.floor(Math.random()*10));
+    else if(Number.isNAN(Number(this.privatekey.substring(0,4)))){
+      return false;
     }
-    key = group1"-"+group2"-"+group3;
-    return key;
+    else if(Number.isNAN(Number(this.privatekey.substring(5,9)))){
+      return false;
+    }
+    else if(Number.isNAN(Number(this.privatekey.substring(10,14)))){
+      return false;
+    }
+    else{
+      return true;
+    }
+    //Static function below this comment.
+    static makePrivateKey(){
+      let key = "";
+      let limit = 14;
+      let group1 = "";
+      let group2 = "";
+      let group3 = "";
+      for(let p = 0; p<4; p++){
+        group1 += String(Math.floor(Math.random()*10));
+        group2 += String(Math.floor(Math.random()*10));
+        group3 += String(Math.floor(Math.random()*10));
+      }
+      key = group1+"-"+group2+"-"+group3;
+      return key;
+    }
   }
-}
